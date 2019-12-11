fa1e63f6655583bc8bc4756fac2022c11bc156a6
Tom Smith <tom@takeontom.com>
2019-12-11 14:30:25 +0000

    Delete existing AADF By Direction data before insert

--- roadtrafficapi/importers.py
+++ roadtrafficapi/importers.py
@@ -7,18 +7,47 @@ from sqlalchemy.orm import backref, relationship, scoped_session, sessionmaker
 from tqdm import tqdm
 
 from . import db
+from .models import AADFByDirection
 from .schemas import aadf_by_direction_schema, list_aadf_by_direction_schema
 
 
 def import_aadf_by_direction(local_authority_id):
     """
     Import of AADF By Direction data for a specific local authority.
+
+    Is safe to run multiple times for the same local authority. Will remove
+    existing AADF By Direction records for the local authority before
+    attempting to insert data. Does not attempt to merge/patch data.
     """
+
+    # Deliberately allowing any exceptions to crash process. Database actions
+    # all occur in transations, so there's no chance of data loss in case of
+    # error; and the exceptions messages will explain issues perfectly fine.
+
     data = get_aadf_by_direction_data(local_authority_id)
-    load_aadf_by_direction_data(data)
+
+    if data:
+        # Delete existing records
+        delete_aadf_by_direction_data(local_authority_id, db.session)
+
+        # Add new records
+        load_aadf_by_direction_data(data, db.session)
+
+        db.session.commit()
 
 
-def load_aadf_by_direction_data(data):
+def delete_aadf_by_direction_data(local_authority_id, session):
+    """
+    Adds deletion of all existing AADF By Direction records for the specified
+    local authority to the supplied session.
+    """
+    q = AADFByDirection.__table__.delete().where(
+        AADFByDirection.local_authority_id == local_authority_id
+    )
+    session.execute(q)
+
+
+def load_aadf_by_direction_data(data, session):
     """
     Save AADF By Direction data into the database.
 
@@ -31,17 +60,13 @@ def load_aadf_by_direction_data(data):
     # so need to actually loop through each row.
     aadf_by_directions = []
     for row in tqdm(data):
-        try:
-            aadf_by_direction = aadf_by_direction_schema.load(row)
-        except ValidationError as e:
-            print(row)
-            print(e)
-            continue
-
+        # May raise validation errors
+        # (`marshmallow.exceptions.ValidationError`). Deliberately not handling
+        # them here and allowing them to bubble.
+        aadf_by_direction = aadf_by_direction_schema.load(row)
         aadf_by_directions.append(aadf_by_direction)
 
-    db.session.bulk_save_objects(aadf_by_directions)
-    db.session.commit()
+    session.bulk_save_objects(aadf_by_directions)
 
 
 def get_aadf_by_direction_data(local_authority_id):
@@ -53,6 +78,8 @@ def get_aadf_by_direction_data(local_authority_id):
     # easily constructable, so not much point moving to config.
     url = f"https://dft-statistics.s3.amazonaws.com/road-traffic/downloads/aadfbydirection/local_authority_id/dft_aadfbydirection_local_authority_id_{local_authority_id}.csv"
 
+    # Deliberately not handling any errors here. Let it crash and inspect
+    # manually.
     csv_stream = urlopen(url)
 
     # CSV files tend to be a few MB, so use a generator (codecs.iterdecode) to
